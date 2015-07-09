65b9f2744167dc73b8a667a906a50c500cb9e69a
kudohamu <kudohamu91086@yahoo.co.jp>
2015-07-09 18:34:13 +0900

    "~/temouch/templates/[引数の拡張子]"内にあるファイルの一覧を取得するとこ
    ろまで

--- temouch.hs
+++ temouch.hs
@@ -1,4 +1,6 @@
 import System.Environment
+import System.FilePath
+import System.Directory
 
 putUsage = putStrLn("Usage: temouch filepath")
 
@@ -8,4 +10,38 @@ main = do
   then putUsage
   else do
     let filepath = head $ args
-    print (filepath)
+    homeDir <- getHomeDirectory
+    contents <-  getDirectoryContents $ homeDir ++ "/temouch/templates/" ++ (getFileExtension filepath)
+    let filtedContents = getFiltedContents contents
+    mapM_ print filtedContents
+    print "which template do you want to use?..."
+    templateNo <- readLn
+    print $ filtedContents !! (templateNo - 1)
+
+{- 
+ >> getFileExtension "abc"
+ >  ""
+ >> getFileExtension "abc.txt"
+ >  "txt"
+ >> getFileExtension "abc.txt.exe"
+ >  "exe"
+ -}
+
+getFileExtension :: String -> String
+getFileExtension = tail . snd . splitExtension
+
+
+
+{-
+ >> getFiltedContents ["hoge", ".", "huga", ".git"]
+ >  ["hoge", "huga"]
+ >> getFiltedContents ["hoge", "huga"]
+ >  ["hoge", "huga"]
+ >> getFiltedContents [".DS_Store", ".svn"]
+ >  []
+-}
+
+ignoreFiles = [".", "..", ".DS_Store", ".git", ".svn"]
+
+getFiltedContents :: [String] -> [String]
+getFiltedContents = filter (\file -> notElem file ignoreFiles)
