01b962a83ce9f681eff085737c46103564d87505
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-15 23:17:01 +0900

    fixed: . -> $

--- src/FileLib.hs
+++ src/FileLib.hs
@@ -1,10 +1,12 @@
 module FileLib where
 
+import System.FilePath
+
 getFileExtension :: String -> String
 getFileExtension = tail . snd . splitExtension
 
 exchangeFileExtension :: String -> String -> String
-exchangeFileExtension filepath ext = (fst . splitExtension filepath) ++ ext
+exchangeFileExtension filepath ext = (fst $ splitExtension filepath) ++ ext
 
 getFiltedContents :: [String] -> [String]
 getFiltedContents = filter (\file -> notElem file [".", "..", ".DS_Store", ".git", ".svn"])
