43d37714ea3f85789c891b37259d3346c9149e5d
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-16 18:17:05 +0900

    added: test about exchangeFileExtension

--- test/FileLibSpec.hs
+++ test/FileLibSpec.hs
@@ -12,3 +12,10 @@ fileLibSpec = do
         fileExtension "foo/bar/baz.txt" `shouldBe` "txt"
       it "連続した拡張子のとき最後の拡張子を返すこと" $ do
         fileExtension "foo/bar/baz.tar.gz" `shouldBe` "gz"
+    describe "exchangeFileExtensionについて" $ do
+      it "拡張子を交換すること" $ do
+        exchangeFileExtension "zip" "foo/bar/baz.tar" `shouldBe` "foo/bar/baz.zip"
+      it "連続した拡張子のとき最後の拡張子を交換すること" $ do
+        exchangeFileExtension "xz" "foo/bar/baz.tar.gz" `shouldBe` "foo/bar/baz.tar.xz"
+      it "拡張子がないとき拡張子を追加だけすること" $ do
+        exchangeFileExtension "rar" "foo/bar/baz" `shouldBe` "foo/bar/baz.rar"
