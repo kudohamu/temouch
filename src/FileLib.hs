b4b660cbf21ed8630185a5a65bf4cef9417727d4
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-17 21:27:02 +0900

    fixed: consider in the absence of ext argument of exchangeFileExtension

--- src/FileLib.hs
+++ src/FileLib.hs
@@ -11,6 +11,7 @@ fileExtension filename = case snd $ splitExtension filename of
                            xs -> tail xs
 
 exchangeFileExtension :: String -> FileName -> FileName
+exchangeFileExtension ""  filePath =  fst $ splitExtension filePath
 exchangeFileExtension ext filepath = (fst $ splitExtension filepath) ++ '.' : ext
 
 filterFiles :: [String] -> [FileName] -> [FileName]
