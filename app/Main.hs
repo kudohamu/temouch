915396146d52707a2a06603b1e326cc210516eb8
kudohamu <kudohamu91086@yahoo.co.jp>
2015-08-18 00:01:37 +0900

    fixed: refactor

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import System.Environment
+
 import Temouch
 
+putUsage = putStrLn "Usage: temouch <FILE_PATH>"
+
 main :: IO ()
-main = temouch
+main = do
+  args <- getArgs
+  if length args == 0 then do
+    putUsage
+  else do
+    temouch $ head args
--- src/Temouch.hs
+++ src/Temouch.hs
@@ -1,70 +1,66 @@
-module Temouch
-    ( 
-    temouch
-    ) where
+module Temouch ( 
+  temouch
+) where
 
+import Control.Applicative
+import qualified Data.Foldable as F hiding (mapM_)
 import System.FilePath
-import System.Environment
-import System.Directory
 import System.IO
+import System.Directory
+import System.Posix.Files
+import System.Posix.Time
+import System.Posix.Types
 import Data.String.Utils
 
 import FileLib
 
-ignoreFile = [".", "..", ".DS_Store", ".git", ".svn"]
+ignoreFiles = [".", "..", ".DS_Store", ".git", ".svn"]
 
-temouch = do
-  args <- getArgs
-  if length args /= 1
-  then putUsage
-  else do
-    let filepath = head $ args
-    homeDir <- getHomeDirectory
-    let templatesDir = homeDir ++ "/temouch/templates/" ++ (fileExtension filepath)
-    contents <-  getDirectoryContents templatesDir
-    let filtedContents = filterFiles ignoreFile contents
-    if length filtedContents == 0
+temouch :: FilePath -> IO ()
+temouch filePath = do
+  -- テンプレートファイル群を取得する
+  let ext = fileExtension filePath
+  tempDir <- (++) <$> getHomeDirectory <*> pure ("/temouch/templates/" ++ ext)
+  exist <- doesDirectoryExist tempDir
+  if exist then do
+    templates <- filterFiles ignoreFiles <$> getDirectoryContents tempDir
+    -- どのテンプレートを使うか
+    putStrLn "You can use these files as template.\n\
+             \ \n\
+             \-----------------------------------------"
+    mapM_ putStrLn . map (\(num, file) -> show num ++ ": " ++ file) $ (0, "don't use") : zip [1..] templates
+    putStrLn "-----------------------------------------\n\
+             \ \n\
+             \Which template do you want to use? Please choose number."
+    templateNo <- readLn :: IO Int
+
+    if templateNo == 0
     then do
-      putStrLn "No templates match. So the file was created as an empty file."
+      -- 空のファイルを作成する
+      touch filePath ""
+      putStrLn $ "Created " ++ filePath ++ " (no using template)."
     else do
-      putStrLn "You can use these files as template."
-      putStrLn ""
-      putStrLn "-----------------------------------------"
-      mapM_ putStrLn . map (\(num, file) -> show num ++ ": " ++ file) $ (0, "no use") : zip [1..] filtedContents
-      putStrLn "-----------------------------------------"
-      putStrLn ""
-      putStrLn "Which template do you want to use? Please choose number."
-      templateNo <- readLn
-      let (newDir, newFile) = splitFileName filepath
-      createDirectoryIfMissing True newDir
-      exist <- doesFileExist filepath
-      let tempPath = if templateNo /= 0 then templatesDir ++ "/" ++ filtedContents !! (templateNo - 1) else ""
-      if exist
-      then do
-        putStrLn "File already exist! Overwrite? [y|n]"
-        overwrite <- getChar
-        if overwrite == 'y'
-        then do
-          createFile tempPath filepath
-        else do
-          putStrLn "Stopped creating."
-      else do
-        createFile tempPath filepath
-
-putUsage = putStrLn("Usage: temouch filepath")
-
-createFile :: String -> String -> IO ()
-createFile "" filePath = do
-  writeFile filePath ""
-  putStrLn $ "Created " ++ filePath ++ " (no using template)."
-createFile tempPath filePath = do
-  writeFile filePath ""
-  templateHandle <- openFile tempPath ReadMode
-  templateContents <- hGetContents templateHandle
-  newFileHandle <- openFile filePath AppendMode
-  mapM_ (\line -> hPutStrLn newFileHandle $ replace "!!!FILE_NAME!!!" (fst . splitExtension . snd $ splitFileName filePath) line) $ lines templateContents
-  hClose templateHandle 
-  hClose newFileHandle
-  putStrLn $ "Created " ++ filePath ++ " (using template '" ++ tempPath ++ "')."
+      let newFilePath = (exchangeFileExtension $ fileExtension (templates !! (templateNo - 1))) filePath
+      template <- readFile $ tempDir ++ "/" ++ templates !! (templateNo - 1)
+      -- メタ変数を置き換えてファイルに書き込む
+      touch newFilePath $ subs template [("!!!FILE_NAME!!!", (fst . splitExtension . snd $ splitFileName newFilePath))]
+      putStrLn $ "Created " ++ newFilePath ++ " (using template '" ++ templates !! (templateNo - 1) ++ "')."
+  else do
+    touch filePath ""
+    putStrLn "No templates match. So the file was created as an empty file."
 
 
+touch :: FilePath -> String -> IO ()
+touch filePath content = do
+    nowTime <- epochTime
+    amtouch filePath content nowTime nowTime
+
+amtouch :: FilePath -> String -> EpochTime -> EpochTime -> IO ()
+amtouch filePath content atime mtime= do
+  exist <- doesFileExist filePath
+  if exist then do
+    -- タイムスタンプ変更
+    setFileTimes filePath atime mtime
+  else do
+    createDirectoryIfMissing True $ fst $ splitFileName filePath
+    writeFile filePath content
